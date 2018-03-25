3c1aac01cedac16be072cc17be1b1e3e0d7d9568
mkorasiak <maciej.korasiak@wp.pl>
2018-03-25 07:52:09 +0200

    Dodaj pliki projektów.

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+/// <summary>
+/// Klasa opisująca dzienniczek studenta z danymi i statystykami
+/// </summary>
+namespace StudentDiary
+{
+    class Diary
+    {
+        //stan - zmienne i pola
+        List<float> ratings;
+
+        //zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+        public float CalculateAvarage()
+        {
+            float Sum=0, Avg=0;
+            foreach (var rating in ratings)
+            {
+                Sum += rating;
+            }
+            return Avg = Sum / ratings.Count;
+
+        }
+    }
+}
